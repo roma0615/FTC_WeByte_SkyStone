a45301bc29451f1ce8ad8f05a6ecf9724b2b9262
Nicholas Pietraszek <Nicholas1328@comcast.net>
2019-12-08 21:30:00 -0600

    Added date comment.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepotSideTensorflowRED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepotSideTensorflowRED.java
@@ -20,7 +20,7 @@ import java.util.*;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
+//12/8/19 demo code idea for tensorflow combination
 @Autonomous(name="DepotSideTensorFlowRED")
 
 public class DepotSideTensorflowRED extends LinearOpMode {
